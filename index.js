ba1deab00ffcc94490881641376473f9e27bfc6f
almasamrenov <almasamrenov24@gmail.com>
2023-10-24 01:55:31 +0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const newsletterButton = document.getElementById("newsletter-button");
+newsletterButton.addEventListener("click", () => {
+  const subscribeModal = document.getElementById("subscribe-modal");
+  if (subscribeModal) {
+    const modal = new bootstrap.Modal(subscribeModal);
+    modal.show();
+  }
+});
+
+const subscribeButton = document.querySelector("#subscribe-button");
+subscribeButton.addEventListener("click", () => {
+  alert("You were subscribed.");
+});
